70470334108206078be1ecfe8c0ecc5cf674b1fe
RossHoyt <rosshoytmusic@gmail.com>
2018-12-18 19:42:19 -0800

    pre-jpa, pre-persistence.xml

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,17 +3,30 @@ package app;
 
 
 
+//import com.fasterxml.classmate.AnnotationConfiguration;
+import com.fasterxml.classmate.AnnotationConfiguration;
 import commandline.pw_utils.PasswordField;
-
+import org.hibernate.*;
+import org.hibernate.cfg.*;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.Scanner;
+import org.hibernate.*;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.internal.SessionFactoryImpl;
+import javax.persistence.*;
+import org.hibernate.cfg.*;
+import org.hibernate.service.ServiceRegistry;
 
 public class Main {
-
-   private static String[] midiSrcFiles = new String[]{"src/main/resources/pianocon.mid", "src/main/resources/la_mer_1.mid", "src/main/resources/helloWorld.mid"};
+   private final String RESOURCES = "src/main/resources/";
+   private final String DRIVER = "com.mysql.jdbc.Driver";
+   private final String CONFIG = "hibernate.cfg.xml";
+   private String[] midiSrcFiles = new String[]{ RESOURCES + "pianocon.mid", RESOURCES + "la_mer_1.mid",
+         RESOURCES + "helloWorld.mid"};
 
    public static void main(String[] args) {
       if(args.length > 0) {
@@ -24,8 +37,57 @@ public class Main {
    }
    public static void commandLineApp2(){
       System.out.println("Welcome to the app midi database V2.0 - \n HIBERNATE EDITION!!!.");
+      //create connnection
+
+
+      SessionFactory sessionFactory;
+
+      sessionFactory = new AnnotationConfiguration()
 
    }
+
+   private static final SessionFactory sessionFactory;
+   static {
+      try {
+         sessionFactory = new AnnotationConfiguration()
+               .configure().buildSessionFactory();
+               .addPackage("test.animals") //the fully qualified package name
+               .addAnnotatedClass(Flight.class)
+               .addAnnotatedClass(Sky.class)
+               .addAnnotatedClass(Person.class)
+               .addAnnotatedClass(Dog.class)
+               .addResource("test/animals/orm.xml")
+               .configure()
+               .buildSessionFactory();
+
+      } catch (Throwable ex) {
+         // Log exception!
+         throw new ExceptionInInitializerError(ex);
+      }
+   }
+
+   public static Session getSession()
+         throws HibernateException {
+      return sessionFactory.openSession();
+   }
+}
+   public Configuration getConfiguration(){
+      // Creating Configuration Instance & Passing Hibernate Configuration File
+      Configuration configObj = new Configuration();
+      configObj.configure(RESOURCES + CONFIG);
+
+   }
+   // Method Used To Create The Hibernate's SessionFactory Object
+   public static SessionFactory getSessionFactory() {
+
+      // Since Hibernate Version 4.x, Service Registry Is Being Used
+      ServiceRegistry serviceRegistryObj = new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build();
+
+      // Creating Hibernate Session Factory Instance
+      SessionFactory factoryObj = configObj.buildSessionFactory(serviceRegistryObj);
+      return factoryObj;
+   }
+
 
    /**
     * Static method which runs simple DAO interation of MIDI DB command line app.
